4ad2a783a0fb2844ca8dc921b302c5142eab0e12
Fatma <fmutlu3002@gmail.com>
2023-12-04 21:12:42 +0300

    navbar and footer done

--- delights-store-client/src/app/components/Menu.tsx
+++ delights-store-client/src/app/components/Menu.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import CartIcon from './CartIcon'
 
 const links =[
   {id: 1, title: "Homepage", url: "/"},
@@ -32,23 +33,28 @@ const Menu = () => {
             onClick={()=>setOpen(false)}/>
         )
         }
-        <div className='bg-red-300 text-white absolute left-0 top-24 h-[calc(100vh-6rem)] 
-        flex flex-col gap-8 w-full items-center justify-center text-3xl z-10'>
-          {
-            links.map((item)=>(
-              <Link key={item.id} href={item.url}>
-                {item.title}
-              </Link>
-            ))
-          }
-          {!user ?(
-            <Link href="/login">Login</Link>
-          ):(
-            <Link href="/orders">Orders</Link>
-          )
-          }
-          <Link href="/cart"></Link>
-        </div>
+        {open &&(
+          <div className='bg-red-300 text-white absolute left-0 top-24 h-[calc(100vh-6rem)] 
+          flex flex-col gap-8 w-full items-center justify-center text-3xl z-10'>
+            {
+              links.map((item)=>(
+                <Link key={item.id} href={item.url} onClick={()=>setOpen(false)}>
+                  {item.title}
+                </Link>
+              ))
+            }
+            {!user ?(
+              <Link href="/login" onClick={()=>setOpen(false)}>Login</Link>
+            ):(
+              <Link href="/orders" onClick={()=>setOpen(false)}>Orders</Link>
+            )
+            }
+            <Link href="/cart" onClick={()=>setOpen(false)}>
+              <CartIcon/>
+            </Link>
+          </div>
+        )
+        }       
     </div>
   )
 }
--- delights-store-client/src/app/components/Navbar.tsx
+++ delights-store-client/src/app/components/Navbar.tsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import Menu from './Menu'
 import Link from 'next/link'
+import Image from 'next/image';
+import CartIcon from './CartIcon';
 
 const Navbar = () => {
+    const user= false;
   return (
-    <div className='h-12 text-red-300 p-4 flex items-center justify-between border-b-2 border-b-red-300 uppercase'>
-        <div>
+    <div className='h-12 text-red-300 p-4 flex items-center justify-between border-b-2
+     border-b-red-300 uppercase md:h-24 lg:px-20 xl:px-40'>
+        <div className='hidden md:flex gap-4 flex-1'>
+            <Link href="/">Home</Link>
+            <Link href="/menu">Menu</Link>
+            <Link href="/">Contact</Link>
+        </div>
+        <div className='md:font-bold flex-1 md:text-center'>
             <Link href="/">Delighto</Link>
         </div>
-        <div>
+        <div className='md:hidden'>
            <Menu/>
         </div> 
+        <div className='hidden md:flex gap-4 items-center flex-1 justify-end'>
+            <div className='md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer
+             bg-pink-200 px-1 rounded-md '>
+                <Image src="/temporary/phone.png" alt='' width={20} height={20}></Image>
+                <span>123 951 48</span>
+            </div>
+            {!user ?(
+                <Link href="/login">Login</Link>
+            ):(
+                <Link href="/orders">Orders</Link>
+            )  
+                    
+        }  
+        <CartIcon/>                        
+        </div>
     </div>
   )
 }
