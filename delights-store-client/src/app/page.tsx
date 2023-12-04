4d6b55edd24de5dccc79d76b1e4ab71244d3e8bb
Fatma <fmutlu3002@gmail.com>
2023-12-04 22:40:44 +0300

    slider component

--- delights-store-client/src/app/page.tsx
+++ delights-store-client/src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from 'next/image'
+// import Featured from "./components/Featured";
+// import Offer from "./components/Offer";
+import Slider from "./components/Slider";
+
 
 export default function Home() {
   return (
-    <main>  </main>
+    <main>
+      <Slider/>
+      {/* <Featured/>
+      <Offer/> */}
+      
+    </main>
   )
 }
