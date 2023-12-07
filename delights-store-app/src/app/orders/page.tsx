17fcfbbd0b390ee359fe4c5fdd839b39411a3b90
Fatma <fmutlu3002@gmail.com>
2023-12-07 10:56:22 +0300

    order page

--- delights-store-app/src/app/orders/page.tsx
+++ delights-store-app/src/app/orders/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import React from "react";
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { OrderType } from "@/types/types";
 import {useSession} from "next-auth/react"
 import { useRouter } from "next/navigation"
+import Image from "next/image";
+
 const OrdersPage = () => {
 
   const {data: session, status} = useSession();
@@ -19,6 +21,32 @@ const OrdersPage = () => {
       fetch("http://localhost:3000/api/orders").then((res) => res.json()),
   });
 
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: string }) => {
+      return fetch(`http://localhost:3000/api/orders/${id}`, {
+        method:"PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(status),
+      });
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["orders"] });
+    },
+  });
+
+  const handleUpdate = (e: React.FormEvent<HTMLFormElement>, id: string) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.elements[0] as HTMLInputElement;
+    const status = input.value;
+
+    mutation.mutate({ id, status });
+    //toast.success("The order status has been changed!")
+  };
 
    if(isLoading || status=== "loading") return 'Loading...'
   return (
@@ -39,8 +67,26 @@ const OrdersPage = () => {
             <td className="hidden md:block py-6 px-1">{item.id}</td>
             <td className="py-6 px-1">{item.createdAt.toString().slice(0, 10)}</td>
             <td className="py-6 px-1">{item.price}</td>
-            <td className="hidden md:block py-6 px-1">Pure Rose Bliss Delight 250 g (1), Sweet Surprise Assortment 250 g (1)</td>
+            <td className="hidden md:block py-6 px-1">{item.products[0].title}</td>
             <td className="py-6 px-1">{item.status}</td>
+            {session?.user.isAdmin ? (
+                <td>
+                  <form
+                    className="flex items-center justify-center gap-4"
+                    onSubmit={(e) => handleUpdate(e, item.id)}
+                  >
+                    <input
+                      placeholder={item.status}
+                      className="p-2 ring-1 ring-red-100 rounded-md"
+                    />
+                    <button className="bg-red-400 p-2 rounded-full">
+                      <Image src="/edit.png" alt="" width={20} height={20} />
+                    </button>
+                  </form>
+                </td>
+              ) : (
+                <td className="py-6 px-1">{item.status}</td>
+              )}
           </tr>
           )}
         </tbody>
